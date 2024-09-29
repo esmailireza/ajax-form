28e6cb5b715ed654525894946c530d0e2d3847f0
reza <esmailireza20@gmail.com>
2024-09-29 19:50:28 +0330

    check isset data

--- login.php
+++ login.php
@@ -1,3 +1,17 @@
 <?php
-var_dump($_POST);
+
+if (isset($_POST['username']) && isset($_POST['password'])) {
+    echo json_encode([
+        'success'=>true,
+        'message'=>'Login Successful',
+        'status'=>200,
+    ]);
+    
+}else{
+    echo json_encode([
+        'success'=>false,
+        'message'=>'Login failed',
+        'status'=>200,
+    ]);
+}
 
